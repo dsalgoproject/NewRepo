24614647fdd3d64070ffdd24295606f77f5fa91a
dsalgoproject <swasthika.cse@gmail.com>
2024-11-15 15:24:41 -0500

    First commit

---
+++ GitCommitTest/src/test/java/gitcommittesting/TestGitCommit.java
@@ -0,0 +1,11 @@
+package gitcommittesting;
+
+import org.testng.annotations.Test;
+
+public class TestGitCommit {
+	@Test
+	public void Test() {
+		System.out.println("Testing Git committ");
+	}
+
+}
